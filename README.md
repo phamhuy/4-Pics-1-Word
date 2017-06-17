f5a2d73de5f30b626e5b1c79d85573c884cc2305
phamhuy <hqpham1@uci.edu>
2017-06-17 00:56:29 -0700

    Trunk Creation.

--- README.md
+++
@@ -1,2 +0,0 @@
-# 4-Pics-1-Word
-Solution for 4 Pics 1 Word game
---
+++ Solution.py
@@ -0,0 +1,51 @@
+"""
+Date Created: June 17, 2017
+Project: 4 Pics 1 Word Solution
+Team Members: Huy Pham, Loc Phan, Minh Huynh
+"""
+
+
+"""
+Function solution
+Argument:
+    im - a screenshot of a puzzle from a phone
+Return:
+    the solution to the puzzle
+    
+Description:
+    This function takes an input as an rgb image screenshot
+    of a puzzle in the game 4-pics-1-word and give a solution
+    to the puzzle
+"""
+def solution(im):
+    # Process image to extract info from the image
+    word_size, letters, pics = process_image(im)
+
+    # Generate words from the word size and the list of letters
+    dictionary = import_dictionary()
+    possible_words = generate_words(dictionary, word_size, letters)
+
+    # Generate labels from the 4 pics
+    labels = generate_labels(pics)
+
+    # Generate solutions
+    possible_solutions = generate_solutions(possible_words, labels)
+
+def process_image(im):
+    pass
+
+def import_dictionary():
+    pass
+
+def generate_words(dictionary, word_size, letters):
+    pass
+
+def generate_solutions(possible_words, labels):
+    pass
+
+def input_image():
+    pass
+
+if __name__ == "__main__":
+    im = input_image()
+    print solution(im)
